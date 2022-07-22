af4913081c6fd4822625e384442aed0e06c9448e
3khati <m3amir88@yahoo.com>
2022-07-22 21:02:43 +0430

    changed!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from "next";
 
-import { HomeHeader, Categories, Products, HomeRequirements } from "components";
+import { HomeHeader, Categories, Products } from "components";
 
 const Home: NextPage = () => {
 	return (
-		<HomeRequirements>
+		<>
 			<HomeHeader />
 			<Categories categories={[]} />{" "}
 			{/* the prop(s) will add in guard component! */}
 			<Products products={[]} isSearchingProducts={false} />{" "}
 			{/* the prop(s) will add in guard component! */}
-		</HomeRequirements>
+		</>
 	);
 };
 
